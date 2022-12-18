6fb5dade995d49c85b70b6fbeffe6bb83446e5f0
jvillad <jyvillad@gmail.com>
2022-12-18 16:49:45 +1100

    Added Carousel component for specific pet

---
+++ src/Carousel.jsx
@@ -0,0 +1,43 @@
+import { Component } from "react";
+
+// class components and hooks do not mix
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleIndexClick = (ev) => {
+    this.setState({
+      active: +ev.target.dataset.index,
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal img" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              onClick={this.handleIndexClick}
+              data-index={index}
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
